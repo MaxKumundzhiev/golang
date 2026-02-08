664e83f186aca09d72d6a6e53a8708f099e5df25
KumundzhievMaxim <37558223+MaxKumundzhiev@users.noreply.github.com>
2026-02-08 12:56:26 +0300

    main, defer & panic;

---
+++ defer.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+
+func main() {
+	/*
+	defer - отложить
+	defer is a keyword as a func or var or others
+	idea of defer is to postpone a call of a particular function to manage the resources
+	отложить выполнение указанной функции до момента завершения окружающей функции. 
+	То есть defer позволяет управлять ресурсами и гарантировать выполнение определённых действий независимо от того, 
+	как будет завершена текущая функция — будь то успешное выполнение или неожиданный выход из-за ошибки.
+
+	data structure used behind is stack aka LIFO queue
+
+	order
+		start
+		end
+		3rd
+		2nd
+		1st
+
+	use cases:
+		- close files, connections, channels
+		- unlock mutexes
+		- log function exit points
+		- handle panics and recover from them
+	*/
+
+	fmt.Println("start")
+
+	defer fmt.Println("1st call")
+	defer fmt.Println("2nd call")
+	defer fmt.Println("3rd call")
+
+	fmt.Println("end")
+}
