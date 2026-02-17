4531caa7830c76ab66eedb15bd9263a67f5e5841
KumundzhievMaxim <37558223+MaxKumundzhiev@users.noreply.github.com>
2026-02-17 09:53:39 +0300

    main, arrays;

--- arrays.go
+++ arrays.go
@@ -2,6 +2,71 @@ package main
 
 import "fmt"
 
+/*
+Массив - фундаментальная структура данных, представляющая собой последовательность
+		 элементов одного типа фиксированной длины.
+
+При объявлении массива сразу указывается размер и это влияет на тип переменной
+array := [3]int{1,2,3}
+fmt.Println(reflect.ValueOf(array).Type().String()) // [3]int
+
+Проинициализировать массив можно разными способами:
+var array1 [3]int // [0 0 0]
+array5 := [3]int{} // [0,0,0]
+array2 := [3]int{1, 2, 3} // [1 2 3]
+array3 := [3]int{1: 2} // [0 2 0]
+array4 := [...]int{1: 2} // [0 2]
+
+
+Массив может принимать любой тип в качестве типа элементов, даже другой массив.
+Так мы можем объявить многомерный массив, где каждым элементом массива будет другой массив:
+
+matrix := [2][2]{
+	{1, 2},
+	{3, 4},
+}
+
+for rowIndex, row := range matrix {
+	for columnIndex, value := range row {
+		fmt.Println("rowIndex %d, columnIndex %d, value %d\n", rowIndex, columnIndex, value)
+	}
+}
+
+
+Практические примеры использования
+- Буферизация данных (array of bytes)
+var buffer [256]bytes
+buffer := [256]bytes
+
+- Фиксированные наборы значений
+var daysOfWeek [7]string = [7]string {
+	"Monday",
+	"Tuesday",
+	...
+}
+
+- Массивы как ключи в map
+// Только если размеры массивов одинаковые
+positions := map[[2]int]string{
+    {0, 0}: "origin",
+    {1, 2}: "target",
+}
+
+to chnage array inside of function
+we need to pass the pointer to the function
+
+func ChangeFirstElement(arr *[3]int) {
+	arr[0] = 100
+}
+
+func main() {
+	array := [3]int{1, 2, 3}
+	ChangeArray(&array)
+	fmt.Println(array)
+}
+
+*/
+
 func main() {
 	// arrays
 	var ArrayA [3]int // [0, 0, 0]
@@ -29,5 +94,8 @@ func main() {
 
 	SliceB = append(SliceB, )
 	
+
+	array := [3]int{}
+	fmt.Println(array)
 }
 
