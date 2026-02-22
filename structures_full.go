9a4ad41a87d2ef0d8e0095549d71de958c30ba3e
KumundzhievMaxim <37558223+MaxKumundzhiev@users.noreply.github.com>
2026-02-22 23:47:23 +0400

    main, structs;

---
+++ structures_full.go
@@ -0,0 +1,28 @@
+package main
+
+/*
+lowercase - private
+uppercase - public
+
+keyword type - means we want to create a new name type
+
+
+type <structname> struct {
+	fields
+	...
+
+anonymys struct - purpose to use it just once 
+we define it using struct keyword and initilise directly
+
+job := struct {
+	title: 	strint
+	salary: float64
+}{
+	title: "Software engineer",
+	salary: 100000
+}
+
+*/
+
+
+func main() {}
